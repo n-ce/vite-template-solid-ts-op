9512bb889b434151d95ef9ff0b0b92f507472fe2
n-ce <abrocks1120@gmail.com>
2025-03-10 20:17:23 +0530

    init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import postcssJitProps from 'postcss-jit-props';
+import OpenProps from 'open-props';
+
+
+export default defineConfig(() => ({
+  base: '/vite-template-island/',
+  plugins: [solidPlugin()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssJitProps(OpenProps)
+      ]
+    }
+  }
+}));
+
+
